20f90eb7092b6cd66dc24b3a692f2c23e23ca9e3
aishwa8141 <a.aish1997@gmail.com>
2019-11-18 12:38:14 +0530

    Gujarati lang support in pdfCalligraph

--- src/main/java/com/example/Converter.java
+++ src/main/java/com/example/Converter.java
@@ -3,17 +3,22 @@ package com.example;
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
+import com.itextpdf.licensekey.LicenseKey;
 
-import java.io.*;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class Converter {
 
     private static void pdfConverter() {
-        File htmlSource = new File("src/main/resources/certificate/index.html");
+        LicenseKey.loadLicenseFile("src/main/resources/itextkey1574056292536_0.xml");
+        File htmlSource = new File("src/main/resources/certificate/language.html");
         File pdfDest = new File("output.pdf");
-        try { ConverterProperties converterProperties = new ConverterProperties();
-            DefaultFontProvider defaultFontProvider = new DefaultFontProvider(false, true, true);
+        try {
+            ConverterProperties converterProperties = new ConverterProperties();
+            DefaultFontProvider defaultFontProvider = new DefaultFontProvider(true, false, false);
             defaultFontProvider.addDirectory("src/main/resources/fonts");
             converterProperties.setFontProvider(defaultFontProvider);
             HtmlConverter.convertToPdf(htmlSource, pdfDest, converterProperties);
